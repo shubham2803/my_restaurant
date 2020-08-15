337ed7aacd579ea6114e7fed830d7e34ef2cf866
shubham2803 <36783620+shubham2803@users.noreply.github.com>
2020-08-15 23:07:54 +0530

    modified profile page

--- src/reservation/models.py
+++ src/reservation/models.py
@@ -7,12 +7,19 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Reservation(models.Model):
+
+    status_choices = [
+        ('COM', 'completed'),
+        ('CAN', 'cancelled'),
+    ]
+
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=50, default='',)
     phone = PhoneNumberField()
     date = models.DateField()
     time = models.TimeField()
     persons = models.IntegerField()
+    status = models.CharField(max_length=15, choices=status_choices)
 
     def __str__(self):
         return self.name
